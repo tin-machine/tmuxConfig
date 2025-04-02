4de4cfe9c112037447efe5eddd3b118997c80641
kaoru.inoue <kaoru.inoue@classi.jp>
2025-04-02 20:17:14 +0900

    ステータスバーを変更

--- shortpath.sh
+++ shortpath.sh
@@ -3,11 +3,16 @@
 # 現在の作業ディレクトリを短縮表示するスクリプトです。
 # ディレクトリの階層が4以上の場合、上位部分を "..." に置き換えて下位2階層のみ表示します。
 
-current_path=$(pwd)
+if [ -n "$1" ]; then
+    current_path="$1"
+else
+    current_path=$(pwd)
+fi
+
 awk -F/ '{
-  if (NF > 3)
-    printf(".../%s/%s", $(NF-1), $NF);
-  else
-    print $0
+    if (NF > 4)
+        printf(".../%s/%s", $(NF-1), $NF);
+    else
+        print $0
 }' <<< "$current_path"
 
